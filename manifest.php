7c273c0e4e3795ac48dd51c252e22bb927de85fd
themefuse <hello@themefuse.com>
2014-11-26 18:40:55 +0200

    first commit

---
+++ manifest.php
@@ -0,0 +1,8 @@
+<?php if (!defined('FW')) die('Forbidden');
+
+$manifest = array();
+
+$manifest['name'] = __('Update', 'fw');
+$manifest['description'] = __('Keep you framework, extensions and theme up to date.', 'fw');
+$manifest['standalone'] = true;
+// todo: add update url
